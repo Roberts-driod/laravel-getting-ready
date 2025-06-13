995d9841c122ef59be2ce8e4ee8c25777463068f
Emils <emils@gmail.com>
2025-06-13 08:43:14 +0300

    route helper

--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -1,6 +1,6 @@
 <h1>Edit book</h1>
 
-<form action="/books/{{ $editBook->id }}/update" method="post">
+<form action="{{route('books.update', $editBook)}}" method="post">
     @csrf
     @method('put')
 
--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,4 +1,4 @@
 <h2>{{ $singleBook->title }}</h2>
 <h3>{{ $singleBook->author }}</h3>
 <p>{{ $singleBook->released_at }}</p>
-<a href="/books">All books</a>
+<a href="{{route('books.index')}}">All books</a>
